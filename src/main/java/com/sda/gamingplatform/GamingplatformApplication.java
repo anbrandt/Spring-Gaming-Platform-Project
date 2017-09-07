509e56d53dcfb5211fac4619142b93edd2330158
Andrzej Brandt <andrzej.brandt@gmail.com>
2017-09-07 18:26:05 +0200

    initial commit

---
+++ src/main/java/com/sda/gamingplatform/GamingplatformApplication.java
@@ -0,0 +1,12 @@
+package com.sda.gamingplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamingplatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamingplatformApplication.class, args);
+	}
+}
