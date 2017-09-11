eb2b332e581b23feb4617352060aa361b56c5720
Andrzej Brandt <andrzej.brandt@gmail.com>
2017-09-11 17:33:00 +0200

    user repository interface - basic CRUD for querying tables in the database

--- src/main/java/com/sda/gamingplatform/config/MvcConfig.java
+++ src/main/java/com/sda/gamingplatform/config/MvcConfig.java
@@ -1,18 +1,23 @@
 package com.sda.gamingplatform.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 
 @Configuration
+@EnableWebMvc
 public class MvcConfig extends WebMvcConfigurerAdapter {
 
+	//tutaj znajdują się odniesienia do podstron bezstanowych - konkretnym urlom przypisujemy podstronę html
 
 	@Override
 	public void addViewControllers(ViewControllerRegistry registry) {
-		registry.addViewController("/index")
-		.setViewName("/index");
+		registry.addViewController("/")
+				.setViewName("index");
+		registry.addViewController("/home")
+				.setViewName("index");
 
 	}
 }
