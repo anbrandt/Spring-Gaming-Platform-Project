4cba90528cd54ccaf94777f8e19dbb0ba6c1ab03
Andrzej Brandt <andrzej.brandt@gmail.com>
2017-09-12 18:53:11 +0200

    game config

---
+++ src/main/java/com/sda/gamingplatform/config/GameConfig.java
@@ -0,0 +1,45 @@
+package com.sda.gamingplatform.config;
+
+import java.math.BigInteger;
+
+/**
+ * Created by andrzej on 12.09.17.
+ */
+public class GameConfig {
+
+	private String gameType;
+	private String chosenField;
+	private BigInteger chips;
+
+	public GameConfig() {}
+
+	public GameConfig(String gameType, String chosenField, BigInteger chips) {
+		this.gameType = gameType;
+		this.chosenField = chosenField;
+		this.chips = chips;
+	}
+
+	public String getGameType() {
+		return gameType;
+	}
+
+	public void setGameType(String gameType) {
+		this.gameType = gameType;
+	}
+
+	public String getChosenField() {
+		return chosenField;
+	}
+
+	public void setChosenField(String chosenField) {
+		this.chosenField = chosenField;
+	}
+
+	public BigInteger getChips() {
+		return chips;
+	}
+
+	public void setChips(BigInteger chips) {
+		this.chips = chips;
+	}
+}
